c33795f44caa6afb9f84e8bcbee5c13f703bee9e
cewilliams <cewilliams@csbsju.edu>
2018-12-30 14:41:26 -0600

    Updates

---
+++ precache-manifest.5c687cc4282db497c8a7d958eb1e224c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghp-nyt/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f22882a983c4487f48cc",
+    "url": "/ghp-nyt/static/js/runtime~main.f22882a9.js"
+  },
+  {
+    "revision": "3cd81bc2ccf9b310a847",
+    "url": "/ghp-nyt/static/js/main.3cd81bc2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ghp-nyt/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3cd81bc2ccf9b310a847",
+    "url": "/ghp-nyt/static/css/main.ecd6b838.chunk.css"
+  },
+  {
+    "revision": "2005005a5532f6bdfad465c236381c58",
+    "url": "/ghp-nyt/index.html"
+  }
+];
